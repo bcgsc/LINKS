3063ba888c2e196b242db728f2758fa12b1e1d4f
Rene <rwarren@bcgsc.ca>
2019-04-24 08:12:27 -0700

    Update scaffoldsToAGP2.pl

--- scaffoldsToAGP2.pl
+++ scaffoldsToAGP2.pl
@@ -1,16 +1,17 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 #Rene Warren
+#https://www.ncbi.nlm.nih.gov/assembly/agp/AGP_Specification/
 #V0.1
 #3 Feb 2017
+#mod 24apr2019
 use strict;
 
-#format that SSAKE/LINKS/ARCS outputs
-#scaffold1,2884845,f2441408z328515k9a0.22m10_r3290314z119047k11a0.18m10_r2489402z151210k16a0.12m10_f2362377z142182k10a0.2m10_f2401086z197138k12a0.16m10_f2345317z259078k19a0.10m10_f2521875Z1508258k19a0.10m10_f2462456z179417
-#
+#format SSAKE/LINKS/ARCS outputs:
+#scaffold1,14000,f34z4000k8a0.3m3_r99Z10000
 #
 if($#ARGV<0){
    print "Script to convert LINKS .scaffolds output into AGPv2\n";
-   die "Usage: $0 <.scaffolds> <arcs -- use this if used the ARCS pipeline>\n";
+   die "Usage: $0 <.scaffolds>\n";
 }
 
 my $type;
@@ -22,22 +23,10 @@ while(<IN>){
    my @a=split(/\,/);
    my @b=split(/\_/,$a[2]);
    my $bigsize=0;
-#   foreach my $component(@b){
-#      if($component=~/^([fr])(\d+)z(\d+)k\d+a\d+\.\d+m(.*)/i){
-#         my ($ori,$id,$sz,$gap)=($1,$2,$3,$4);
 
-#         if($gap<0){
-#            $gap=100;
-#         }
-#	 $bigsize += ($sz + $gap);
-#      }elsif($component=~/^([fr])(\d+)z(\d+)$/i){
-#         my ($ori,$id,$sz,$gap)=($1,$2,$3);
-#         $bigsize += ($sz);
-#      }
-#   }
    my $ct=0;
    my ($begin,$end,$size)=(1,0,0);
-
+   my $el=0;
    foreach my $component(@b){
       my ($ori,$id,$type,$sz,$gap) = ("NA","NA","N",0,"NA");
       $ct++;
@@ -60,14 +49,31 @@ while(<IN>){
             $ori="-";
          }
       }
-      $end = $begin + $sz;
-      if($ARGV[1] eq "arcs"){
-         print "$a[0]\t$begin\t$end\t$ct\tU\t$id\t100\tNA\tscaffold\tNA\tyes\t$ori\tpaired-ends\n";
-         $begin = $end + 100;
-      }else{
-	 print "$a[0]\t$begin\t$end\t$ct\t$type\t$id\t$gap\tNA\tscaffold\tNA\tyes\t$ori\tpaired-ends\n";
-         $begin = $end + $gap;
+
+      #abyss-fatoagp
+      #scaffold1	1	4000	1	W	contigtest_0	1	4000	+
+      #scaffold1	4001	4003	2	N	3	scaffold	yes	paired-ends
+      #scaffold1	4004	14003	3	W	contigtest_1	1	10000	+
+  
+      #this script:
+      #scaffold1	1	4000	1	W	oriscaffold_34	1	4000	+
+      #scaffold1	4001	4003	2	N	3	scaffold	yes	paired-ends
+      #scaffold1	4004	14003	3	W	oriscaffold_99	1	10000	-           (this will match the orientation infered by LINKS scaffolder)
+
+
+      $end = $begin + $sz - 1;
+
+      print "$a[0]\t$begin\t$end\t$ct\tW\toriscaffold_$id\t1\t$sz\t$ori\n";###object (ie. scaffold)
+
+      if($b[$el+1] ne ""){###component (ie. gap)
+         $ct++;
+         my $gapstart = $end+1;
+         my $gapend = $gap + $gapstart - 1;
+	 print "$a[0]\t$gapstart\t$gapend\t$ct\tN\t$gap\tscaffold\tyes\tpaired-ends\n";
+         $begin = $gapend + 1;
       }
+
+      $el++;
    }
 }
 close IN;
