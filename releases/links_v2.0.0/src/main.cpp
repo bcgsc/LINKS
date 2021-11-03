2d85c5c21d37a4194d9a7f5c82d0bc059ae73c68
MurathanGoktas <murathangoktas@gmail.com>
2021-11-03 08:58:35 -0700

    release file name changed to v2.0.0

---
+++ releases/links_v2.0.0/src/main.cpp
@@ -0,0 +1,43 @@
+#include "LINKS.hpp"
+#include "InputParser.hpp"
+#include <chrono>
+#include <stdlib.h> // for sleep
+
+int main(int argc, char** argv){
+    InputParser* linksArgParser = new InputParser(argc, argv);
+    if(!linksArgParser->arguments_satisfied){return -1;}
+    linksArgParser->print_opts();
+    LINKS* links = new LINKS(linksArgParser);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    links->init_bloom_filter();
+    auto finish = std::chrono::high_resolution_clock::now();
+    auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(finish-start);
+    std::cout << milliseconds.count() << " ms\n";
+
+    std::cout << "Read fasta stage\n";
+    start = std::chrono::high_resolution_clock::now();
+    links->start_read_fasta();
+    std::cout << "Read fasta stage finish\n";
+    finish = std::chrono::high_resolution_clock::now();
+    milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(finish-start);
+    std::cout << "Read fasta stage took: " << milliseconds.count() << " ms\n";
+
+    std::cout << "Read contig stage\n";
+    start = std::chrono::high_resolution_clock::now();
+    links->start_read_contig();
+    finish = std::chrono::high_resolution_clock::now();
+    milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(finish-start);
+    std::cout << "Read contig stage took: " << milliseconds.count() << " ms\n";
+
+    std::cout << "Pair contigs stage\n";
+    start = std::chrono::high_resolution_clock::now();
+    links->pair_contigs();
+    finish = std::chrono::high_resolution_clock::now();
+    milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(finish-start);
+    std::cout << "Pair contigs stage took: " << milliseconds.count() << " ms\n";
+
+    delete(linksArgParser);
+    delete(links);
+    return 0;
+}
